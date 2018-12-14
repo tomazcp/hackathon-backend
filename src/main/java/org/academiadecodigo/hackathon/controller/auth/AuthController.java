8716b993474e47c20a2a19f0ef55d37fa1850acb
Geraldo_Sem_Pavor <afonsonunes95@gmail.com>
2018-12-14 00:23:51 +0000

    415 fixed

--- src/main/java/org/academiadecodigo/hackathon/controller/auth/AuthController.java
+++ src/main/java/org/academiadecodigo/hackathon/controller/auth/AuthController.java
@@ -9,15 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * REST controller responsible for {@link Patient} related CRUD operations
  */
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/auth")
 public class AuthController {
 
